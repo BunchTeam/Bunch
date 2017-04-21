40a98c26ae27534e4ce6e8e4cfd28e5dce8f2314
Wojti <Wojti@ThinkPad>
2017-04-21 10:02:46 +0200

    test

---
+++ src/java/com/jsf/dao/LoginDAO.java
@@ -0,0 +1,35 @@
+package com.jsf.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.jsf.util.DataConnect;
+
+public class LoginDAO {
+
+	public static boolean validate(String user, String password) {
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataConnect.getConnection();
+			ps = con.prepareStatement("Select login, haslo from uzytkownicy where login = ? and haslo = ?");
+			ps.setString(1, user);
+			ps.setString(2, password);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				return true;
+			}System.out.println("Bledne dane");
+		} catch (SQLException ex) {
+			System.out.println("Login error -->" + ex.getMessage());
+			return false;
+		} finally {
+			DataConnect.close(con);
+		}
+		return false;
+	}
+}
---
+++ src/java/com/jsf/util/DataConnect.java
@@ -0,0 +1,31 @@
+package com.jsf.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataConnect {
+
+	public static Connection getConnection() {
+		try {
+			//Class.forName("com.mysql.jdbc.Driver");
+			//Connection con = DriverManager.getConnection(
+			//		"jdbc:mysql://localhost:3306/cardb", "pankaj", "pankaj123");
+                        
+                        Class.forName("org.postgresql.Driver");
+Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5433/facebook","postgres", "postgres");
+
+			return con;
+		} catch (Exception ex) {
+			System.out.println("Database.getConnection() Error -->"
+					+ ex.getMessage());
+			return null;
+		}
+	}
+
+	public static void close(Connection con) {
+		try {
+			con.close();
+		} catch (Exception ex) {
+		}
+	}
+}

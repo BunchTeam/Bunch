49bd6f676b3f07bb22cddccc9bef049294ffe6d4
Wojti <Wojti@ThinkPad>
2017-04-25 12:30:43 +0200

    logowanie

--- src/java/com/jsf/dao/LoginDAO.java
+++ src/java/com/jsf/dao/LoginDAO.java
@@ -6,30 +6,36 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.jsf.util.DataConnect;
+import db.Uzytkownicy;
 
 public class LoginDAO {
 
-	public static boolean validate(String user, String password) {
-		Connection con = null;
-		PreparedStatement ps = null;
+    public static boolean validate(String user, String password) {
+        Connection con = null;
+        PreparedStatement ps = null;
 
-		try {
-			con = DataConnect.getConnection();
-			ps = con.prepareStatement("Select login, haslo from uzytkownicy where login = ? and haslo = ?");
-			ps.setString(1, user);
-			ps.setString(2, password);
+        try {
+            con = DataConnect.getConnection();
+            ps = con.prepareStatement("Select login, haslo, imie, nazwisko from uzytkownicy where login = ? and haslo = ?");
+            ps.setString(1, user);
+            ps.setString(2, password);
 
-			ResultSet rs = ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
 
-			if (rs.next()) {
-				return true;
-			}System.out.println("Bledne dane");
-		} catch (SQLException ex) {
-			System.out.println("Login error -->" + ex.getMessage());
-			return false;
-		} finally {
-			DataConnect.close(con);
-		}
-		return false;
-	}
+            if (rs.next()) {
+         //      String imie = rs.getString("imie");
+           //    String nazwisko = rs.getString("nazwisko");
+             //   Uzytkownicy.setImie(imie);
+               // Uzytkownicy.setNazwisko(nazwisko);
+                return true;
+            }
+            System.out.println("Bledne dane");
+        } catch (SQLException ex) {
+            System.out.println("Login error -->" + ex.getMessage());
+            return false;
+        } finally {
+            DataConnect.close(con);
+        }
+        return false;
+    }
 }
